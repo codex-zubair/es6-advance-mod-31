bbf3be76e9359489b5836a9ca1a0f35c995c498e
Rocko <codingwithrock@gmail.com>
2022-08-25 19:19:50 +0600

    prototype object system are clear i can now connect with object with each other

--- object-class.js
+++ object-class.js
@@ -1,5 +1,22 @@
+// Another class from here we will call prototype Constructor...
+class MatchHouse {
+    id;
+    workShift;
+    mustCleanDay = 'Friday';
+
+    constructor(id, workShift) {
+        this.id = id;
+        this.workShift = workShift;
+    }
+
+}
+
+
+
+
+
 // creating a class...
-class MatchMember {
+class MatchMember extends MatchHouse {
 
     // declaring the variables for this class...
     name;
@@ -11,16 +28,32 @@ class MatchMember {
 
 
     // now creating a constructor for taking user values...
-    constructor(name, age, location, job) {
+    constructor(name, age, location, job, id, workShift, mustCleanDay) {
+
+
+        // !Super must stay on top...
+        super(id, workShift, mustCleanDay);
         this.name = name;
         this.age = age;
         this.location = location;
         this.job = job;
+
+
+
+        // ! here using super constructor from another class 
+        // ! it called prototype (get features from one another class.)
+        /*!    Prototypes are the mechanism by which JavaScript objects 
+           inherit or get features from one another. */
+
+
+
+
+
     }
     // Now Constructor are ready...
 
 
-    // Now Creating function for work...
+    //! Now Creating function for work...
     introDuction(name, age, location, job) {
         console.log(`my name is ${name} 
         i'm ${age}
@@ -30,7 +63,12 @@ class MatchMember {
     }
 
 
-
+    // inherited feature (prototype from another place)
+    myCleanDay(id, workShift) {
+        console.log(`my id is ${id}
+i'll clean ${this.mustCleanDay}
+at ${workShift}`);
+    }
 
 
 }
@@ -39,4 +77,11 @@ class MatchMember {
 const younus = new MatchMember();
 
 
+
+// normally calling object in class..
 younus.introDuction('younus', 25, "dhaka", "teacher");
+
+
+
+//!inheritance object calling...
+younus.myCleanDay(1243, 'Evening')
